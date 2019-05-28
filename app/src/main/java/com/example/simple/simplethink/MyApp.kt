d2b8601ae03265e87587fe583392850e44195478
jiessie <jiessie@jiessiedeMacBook-Pro.local>
2019-05-28 23:51:40 +0800

    upload file

---
+++ app/src/main/java/com/example/simple/simplethink/MyApp.kt
@@ -0,0 +1,12 @@
+package com.example.simple.simplethink
+
+import android.app.Application
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
---
+++ app/src/main/java/com/example/simple/simplethink/base/BaseActivity.kt
@@ -0,0 +1,13 @@
+package com.example.simple.simplethink.base
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by jiessie on 2019/5/28.
+ */
+class BaseActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
