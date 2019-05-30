e96bffb077351dbfd3804d516e369323394459b9
andy <305541315@qq.com>
2019-05-30 19:32:53 +0800

    login page ui1

---
+++ app/src/main/java/com/example/simple/simplethink/login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.simple.simplethink.login
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by mobileteam on 2019/5/30.
+ */
+class LoginActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+      //  setContentView(R.layout.)
+    }
+}
