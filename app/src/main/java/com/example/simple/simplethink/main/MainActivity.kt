1c14124344f32b25c65031f9ab0ecebd7e567a65
andy <305541315@qq.com>
2019-06-03 18:27:19 +0800

    add totle page ui

---
+++ app/src/main/java/com/example/simple/simplethink/main/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.simple.simplethink.main
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.simple.simplethink.R
+
+/**
+ * Created by mobileteam on 2019/6/3.
+ */
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/simple/simplethink/totle/TotleActivity.kt
@@ -0,0 +1,13 @@
+package com.example.simple.simplethink.totle
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by mobileteam on 2019/6/3.
+ */
+class TotleActivity: Activity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
