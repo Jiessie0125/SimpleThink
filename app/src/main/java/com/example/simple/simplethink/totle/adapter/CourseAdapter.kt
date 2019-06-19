39b5efd3b55595a0ff5c54e83e2182b58b935d51
andy <305541315@qq.com>
2019-06-19 19:04:53 +0800

    add course

---
+++ app/src/main/java/com/example/simple/simplethink/totle/adapter/CourseAdapter.kt
@@ -0,0 +1,45 @@
+package com.example.simple.simplethink.totle.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.simple.simplethink.R
+import com.example.simple.simplethink.model.TotleItem
+import java.util.ArrayList
+
+/**
+ * Created by jiessie on 2019/6/11.
+ */
+class CourseAdapter( val totleLish : ArrayList<TotleItem>) : RecyclerView.Adapter<CourseViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return totleLish?.size!!
+    }
+
+    override fun onBindViewHolder(holder: CourseViewHolder?, position: Int) {
+        holder?.mTotleItem?.text = totleLish?.get(position)?.totleItemTxt
+        holder?.mItemImage?.setImageBitmap(totleLish?.get(position)?.totleItemImage)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): CourseViewHolder? {
+        val layoutInflater = LayoutInflater.from(parent?.context)
+        val holder = layoutInflater.inflate(R.layout.course_item, null, false)
+        return CourseViewHolder(holder)
+    }
+
+
+}
+
+class CourseViewHolder(view : View?): RecyclerView.ViewHolder(view) {
+    var mTotleItem : TextView?
+    var mItemImage : ImageView?
+
+    init {
+        mTotleItem = view?.findViewById(R.id.recycle_course_tv)
+        mItemImage = view?.findViewById(R.id.recycle_course_img)
+    }
+
+}
--- app/src/main/java/com/example/simple/simplethink/totle/fragment/TotleFragment.kt
+++ app/src/main/java/com/example/simple/simplethink/totle/fragment/TotleFragment.kt
@@ -14,6 +14,7 @@ import com.example.simple.simplethink.R
 import com.example.simple.simplethink.model.TotleItem
 import com.example.simple.simplethink.model.TotleSortResponse
 import com.example.simple.simplethink.netapi.HttpResposityImpl
+import com.example.simple.simplethink.totle.adapter.CourseAdapter
 import com.example.simple.simplethink.totle.adapter.TotleAdapter
 import com.example.simple.simplethink.utils.FilesUtils
 import com.example.simple.simplethink.utils.ResourcesUtils
@@ -26,18 +27,27 @@ import java.util.*
  */
 class TotleFragment : Fragment(),TotleContact.View {
 
+    init {
+
+    }
+
     lateinit var persenter : TotleContact.Presenter
     var totleList = ArrayList<TotleItem>()
 
     var itemImage : Bitmap ?= null
     lateinit var totleAdapter : TotleAdapter
+    lateinit var courseAdapter : CourseAdapter
 
     override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return inflater!!.inflate(R.layout.fragment_totle,container,false)
     }
 
-    override fun onAttach(context: Context?) {
-        super.onAttach(context)
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        for(i in 0 until 8){
+            var totleItem = TotleItem("1",BitmapFactory.decodeResource(ResourcesUtils.resource,R.drawable.download))
+            totleList.add(totleItem)
+        }
     }
 
     override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
@@ -49,12 +59,9 @@ class TotleFragment : Fragment(),TotleContact.View {
         /*val httpResposityImpl = HttpResposityImpl()
         persenter = TotlePresenter(httpResposityImpl,this)
         persenter.getTotleSort()*/
-
-        for(i in 0 until 8){
-            var totleItem = TotleItem("1",BitmapFactory.decodeResource(ResourcesUtils.resource,R.drawable.download))
-            totleList.add(totleItem)
-        }
         setAdapter()
+        setCourseAdapter()
+
     }
     fun createFragment(): TotleFragment {
         val bundle = Bundle()
@@ -84,5 +91,11 @@ class TotleFragment : Fragment(),TotleContact.View {
         totleAdapter = TotleAdapter(totleList)
         recycle_tv.layoutManager = GridLayoutManager(this.context,4)
         recycle_tv.adapter = totleAdapter
+    }
+
+    private fun setCourseAdapter(){
+        courseAdapter = CourseAdapter(totleList)
+        recycle_course_tv.layoutManager = GridLayoutManager(this.context,2)
+        recycle_course_tv.adapter = courseAdapter
     }
 }
