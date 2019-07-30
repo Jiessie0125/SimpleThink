f787a4b7dd77756a3c54d47e5c17c848a962b7f3
Jiessie0125 <947460633@qq.com>
2019-07-30 19:41:22 +0800

    add potocol page

---
+++ app/src/main/java/com/example/simple/simplethink/login/ProtocolActivity.java
@@ -0,0 +1,38 @@
+package com.example.simple.simplethink.login;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.View;
+import android.webkit.WebView;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import com.example.simple.simplethink.R;
+
+/**
+ * Created by mobileteam on 2019/7/30.
+ */
+
+public class ProtocolActivity extends Activity implements View.OnClickListener {
+
+    private ImageView back;
+    private WebView webView;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_protocol);
+        back = (ImageView) findViewById(R.id.protocol_back_btn);
+        back.setOnClickListener(this);
+        webView = (WebView) findViewById(R.id.protocol_webView);
+        webView.loadUrl("http://www.simplemeditation.cn/privacy/");
+    }
+
+    @Override
+    public void onClick(View view) {
+        finish();
+    }
+
+
+}
