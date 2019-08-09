2d992735865d1dc413c168fd7a083376e411f980
Jiessie0125 <947460633@qq.com>
2019-08-09 16:25:37 +0800

    add photo select

--- app/src/main/java/com/example/simple/simplethink/vip/VIPCenterActivity.kt
+++ app/src/main/java/com/example/simple/simplethink/vip/VIPCenterActivity.kt
@@ -2,9 +2,11 @@ package com.example.simple.simplethink.vip
 
 import android.content.Context
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.v7.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 import com.example.simple.simplethink.R
 import com.example.simple.simplethink.base.BaseActivity
@@ -12,11 +14,14 @@ import com.example.simple.simplethink.login.LoginActivity
 import com.example.simple.simplethink.model.BuzzyCourseResponse
 import com.example.simple.simplethink.model.VIPItem
 import com.example.simple.simplethink.netapi.HttpResposityImpl
+import com.example.simple.simplethink.utils.DateUtils
 import com.example.simple.simplethink.utils.ResourcesUtils
 import com.example.simple.simplethink.utils.auth.AuthInstance
 import kotlinx.android.synthetic.main.activity_vip_center.*
 import kotlinx.android.synthetic.main.fragment_main_postlogon.*
 import kotlinx.android.synthetic.main.title_tool.*
+import me.iwf.photopicker.PhotoPicker
+import java.io.File
 
 /**
  * Created by mobileteam on 2019/6/21.
@@ -72,6 +77,26 @@ class VIPCenterActivity : BaseActivity(), VIPCenterContact.View {
             persenter.getSubscription(AuthInstance.getInstance().accessToken!!)
         }
 
+        user_avatar.setOnClickListener {
+            PhotoPicker.builder()
+                    .setPhotoCount(1)
+                    .setShowCamera(true)
+                    .setShowGif(false)
+                    .setPreviewEnabled(false)
+                    .start(this, PhotoPicker.REQUEST_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK && data != null) {
+            if (requestCode == PhotoPicker.REQUEST_CODE) {
+                val photos = data.getStringArrayListExtra(PhotoPicker.KEY_SELECTED_PHOTOS)
+                val photoPath = photos[0]
+                val inputUri = Uri.fromFile(File(photoPath))
+                Glide.with(this).load(inputUri).apply(RequestOptions().placeholder(R.drawable.photo)).into(user_avatar)
+            }
+        }
     }
 
 
